7d61dd3c3cb8816d18bbe31c5223d27672085737
guillerglez88 <guillerglez88@gmail.com>
2022-08-14 16:12:51 +0200

    formatting test data

--- tests/CsharpDataOriented.UnitTests/ClassAsCollectionTests.cs
+++ tests/CsharpDataOriented.UnitTests/ClassAsCollectionTests.cs
@@ -12,22 +12,16 @@ namespace CsharpDataOriented.UnitTests;
 
 public class ClassAsCollectionTests
 {
-    private readonly object person = new
-    {
-        Name = new
-        {
+    private readonly object person = new {
+        Name = new {
             Given = new[] { "Glen", "Ruben" },
-            Family = "Rodriguez"
-        },
+            Family = "Rodriguez" },
         DoB = new DateTime(2011, 10, 3),
-        Addresses = new[] {
-                new {
-                    Lines = new[] { "184 #42301, esq 423" },
-                    PostalCode = "18100",
-                    Period = new { Start = new DateTime(2019, 04, 17) }
-                }
-            }
-    };
+        Addresses = new[] { 
+            new { 
+                Lines = new[] { "184 #42301, esq 423" },
+                PostalCode = "18100",
+                Period = new { Start = new DateTime(2019, 04, 17) } } } };
 
     [Fact]
     public void CanListClassProps()
