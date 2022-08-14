6f2374feaaf6234bf5d51b2723c7f245649394bc
guillerglez88 <guillerglez88@gmail.com>
2022-08-14 14:36:34 +0200

    cleaning up tests

--- tests/CsharpDataOriented.UnitTests/ClassAsCollectionTests.cs
+++ tests/CsharpDataOriented.UnitTests/ClassAsCollectionTests.cs
@@ -17,21 +17,23 @@ namespace CsharpDataOriented.UnitTests
             var obj = new { 
                 Name = new
                 {
-                    Given = new[] { "Tania", "Mirllan" },
-                    Family = "Gonzalez"
+                    Given = new[] { "Glen", "Ruben" },
+                    Family = "Rodriguez"
                 }, 
-                DoB = new DateTime(1969, 10, 16)
+                DoB = new DateTime(2011, 10, 3)
             };
 
             var seq = Seq(obj.GetType());
 
             var objColl = seq(obj);
+
+            Assert.Equal(2, objColl.Count());
         }
 
         [Fact]
         public void CanRecognizeSeqTypes()
         {
-            var nameArr = new[] { "Tania", "Mirllan" };
+            var nameArr = new[] { "Glenis", "Mayra" };
             var nameList = nameArr.ToList();
             var nameDict = nameArr.ToDictionary(n => n);
 
@@ -39,12 +41,5 @@ namespace CsharpDataOriented.UnitTests
             Assert.True(LeaveAsIs(nameList));
             Assert.True(LeaveAsIs(nameDict));
         }
-    }
-
-    public class Person
-    {
-        public string Name { get; set; }
-        public int Id { get; set; }
-        public DateTime DoB { get; set; }
     }
 }
